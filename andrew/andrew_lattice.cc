adc90ccaf27555dba582fac2f48bc77c71be93ca
scottpratt <prattsc@msu.edu>
2024-09-27 20:58:31 -0400

    yada yada

--- andrew/andrew_lattice.cc
+++ andrew/andrew_lattice.cc
@@ -18,37 +18,52 @@ int main(){
 	CparameterMap parmap;
 	parmap.set("MSU_SAMPLER_SFDIRNAME",string("../progdata/resinfo/spectralfunctions"));
 	parmap.set("RESONANCES_INFO_FILE",string("../progdata/resinfo/pdg-SMASH.dat"));
-	int iT0,iT1,iT;
-	char dummy[200];
-	int iepsilon,NT;
-	double dE,dele=0.005,T0,T1,chi0,chi1;
-	const double HBARC3=pow(HBARC,3);
-	double Tread,chiread,error,epsilon_target;
-	vector<double> Tlattice,chilattice_uu,chilattice_ud,chilattice_us,chilattice_ss;
-	vector<double> Tvec,PvsTvec,EpsilonvsTvec,SvsTvec,ChivsTvec;
+	int iT0,iT1,iepsilon;
+	char dummy[300];
+	double dE,dele,T0,T1,chi0,chi1,dT=1.0,epsilon_target;
+	double Tread,chiread,error;
+	vector<double> Tlattice,chilattice_uu,chilattice_ud,chilattice_us,chilattice_ss,slattice,elattice;
+	vector<double> TvsTvec,Tvec,PvsTvec,EpsilonvsTvec,SvsTvec,ChivsTvec;
 	FILE *fptr;
-	const int NE=1001;
+	const int NE=8169;
+	dele=0.005;
 	Tlattice.resize(NE);
 	chilattice_uu.resize(NE);
 	chilattice_ud.resize(NE);
 	chilattice_us.resize(NE);
 	chilattice_ss.resize(NE);
+	slattice.resize(NE);
+	elattice.resize(NE);
+	
 
 	ChivsTvec.clear(); EpsilonvsTvec.clear();
 	GetLatticeEoS(PvsTvec,SvsTvec,EpsilonvsTvec);
-	NT=PvsTvec.size();
 	
-	for(iT=0;iT<=NT;iT++){
-		SvsTvec[iT]=SvsTvec[iT]/HBARC3;
+	for(iepsilon=0;iepsilon<NE;iepsilon++){
+		elattice[iepsilon]=iepsilon*dele;
+		T0=-dT;
+		iT0=-1;
+		do{
+			iT0+=1;
+			iT1=iT0+1;
+			T0+=dT;
+			T1=T0+dT;
+		}while(EpsilonvsTvec[iT1]<elattice[iepsilon]);
+		dE=EpsilonvsTvec[iT1]-EpsilonvsTvec[iT0];
+		Tlattice[iepsilon]=(EpsilonvsTvec[iT1]-elattice[iepsilon])*T0/dE+(elattice[iepsilon]-EpsilonvsTvec[iT0])*T1/dE;
+		Tlattice[iepsilon]=Tlattice[iepsilon]/1000.0;
+		slattice[iepsilon]=(EpsilonvsTvec[iT1]-elattice[iepsilon])*SvsTvec[iT0]/dE+(elattice[iepsilon]-EpsilonvsTvec[iT0])*SvsTvec[iT1]/dE;
 	}
-	
+	Tlattice[0]=0.0;
+
+///////////////////////////////////////////////	
 	// Now read in lattice data for chi_uu
 	printf("----- chi_uu --------\n");
 	fptr=fopen("latticedata_claudia/chi_uu.dat","r");
 	Tvec.clear();
 	do{
 		fscanf(fptr,"%lf %lf %lf",&Tread,&chiread,&error);
-		fgets(dummy,200,fptr);
+		fgets(dummy,300,fptr);
 		if(!feof(fptr) && Tread<=400.00001){
 			Tvec.push_back(Tread/1000.0);
 			chiread=chiread*pow(Tread/HBARC,3);
@@ -58,30 +73,32 @@ int main(){
 	fclose(fptr);
 	
 	for(iepsilon=0;iepsilon<NE;iepsilon++){
-		epsilon_target=dele*iepsilon;
+		//printf("Ttarget=%g, e=%g\n",Tlattice[iepsilon],iepsilon*dele);
 		iT0=-1;
 		do{
 			iT0+=1;
 			iT1=iT0+1;
-		}while(EpsilonvsTvec[iT1]<epsilon_target);
+		}while(Tvec[iT1]<Tlattice[iepsilon]);
 		T0=Tvec[iT0];
-		T1=Tvec[iT0];
+		T1=Tvec[iT1];
 		chi0=ChivsTvec[iT0];
 		chi1=ChivsTvec[iT1];
-		dE=EpsilonvsTvec[iT1]-EpsilonvsTvec[iT0];
-		Tlattice[iepsilon]=(T0*(EpsilonvsTvec[iT1]-epsilon_target)+T1*(epsilon_target-EpsilonvsTvec[iT0]))/dE;
-		chilattice_uu[iepsilon]=(chi0*(EpsilonvsTvec[iT1]-epsilon_target)+chi1*(epsilon_target-EpsilonvsTvec[iT0]))/dE;
+		dT=Tvec[iT1]-Tvec[iT0];
+		chilattice_uu[iepsilon]=(chi0*(Tvec[iT1]-Tlattice[iepsilon])+chi1*(Tlattice[iepsilon]-Tvec[iT0]))/dT;
+		//printf("epsilon=%g: iT0=%d, iT1=%d, T0=%g, T1=%g, Tlattice=%g, chi0=%g, chi1=%g, chi=%g\n",
+		//elattice[iepsilon],iT0,iT1,T0,T1,Tlattice[iepsilon],chi0,chi1,chilattice_uu[iepsilon]);
 	}
+
 	
 ///////////////////////////////////////////////
 		// Now read in lattice data for chi_ud
 	
 	printf("----- chi_ud --------\n");
 	fptr=fopen("latticedata_claudia/c2ud.cont","r");
-	Tvec.clear();
+	Tvec.clear(); ChivsTvec.clear();
 	do{
 		fscanf(fptr,"%lf %lf %lf",&Tread,&chiread,&error);
-		fgets(dummy,200,fptr);
+		fgets(dummy,300,fptr);
 		if(!feof(fptr) && Tread<=400.00001){
 			Tvec.push_back(Tread/1000.0);
 			chiread=chiread*pow(Tread/HBARC,3);
@@ -90,33 +107,32 @@ int main(){
 	}while(!feof(fptr) && Tread<400.001);
 	fclose(fptr);
 	
-	
 	for(iepsilon=0;iepsilon<NE;iepsilon++){
-		epsilon_target=dele*iepsilon;
+		//printf("Ttarget=%g, e=%g\n",Tlattice[iepsilon],iepsilon*dele);
 		iT0=-1;
 		do{
 			iT0+=1;
 			iT1=iT0+1;
-		}while(EpsilonvsTvec[iT1]<epsilon_target);
+		}while(Tvec[iT1]<Tlattice[iepsilon]);
 		T0=Tvec[iT0];
 		T1=Tvec[iT1];
 		chi0=ChivsTvec[iT0];
 		chi1=ChivsTvec[iT1];
-		dE=EpsilonvsTvec[iT1]-EpsilonvsTvec[iT0];
-		Tlattice[iepsilon]=(T0*(EpsilonvsTvec[iT1]-epsilon_target)+T1*(epsilon_target-EpsilonvsTvec[iT0]))/dE;
-		chilattice_ud[iepsilon]=(chi0*(EpsilonvsTvec[iT1]-epsilon_target)+chi1*(epsilon_target-EpsilonvsTvec[iT0]))/dE;
+		dT=Tvec[iT1]-Tvec[iT0];
+		chilattice_ud[iepsilon]=(chi0*(Tvec[iT1]-Tlattice[iepsilon])+chi1*(Tlattice[iepsilon]-Tvec[iT0]))/dT;
+		//printf("epsilon=%g: iT0=%d, iT1=%d, T0=%g, T1=%g, Tlattice=%g, chi0=%g, chi1=%g, chi=%g\n",
+		//elattice[iepsilon],iT0,iT1,T0,T1,Tlattice[iepsilon],chi0,chi1,chilattice_ud[iepsilon]);
 	}
-	
 	
 ///////////////////////////////////////////////
 		// Now read in lattice data for chi_us
 	
 	printf("----- chi_us --------\n");
 	fptr=fopen("latticedata_claudia/c2us.cont","r");
-	Tvec.clear();
+	Tvec.clear(); ChivsTvec.clear();
 	do{
 		fscanf(fptr,"%lf %lf %lf",&Tread,&chiread,&error);
-		fgets(dummy,200,fptr);
+		fgets(dummy,300,fptr);
 		if(!feof(fptr) && Tread<=400.00001){
 			Tvec.push_back(Tread/1000.0);
 			chiread=chiread*pow(Tread/HBARC,3);
@@ -126,30 +142,32 @@ int main(){
 	fclose(fptr);
 	
 	for(iepsilon=0;iepsilon<NE;iepsilon++){
-		epsilon_target=dele*iepsilon;
+		//printf("Ttarget=%g, e=%g\n",Tlattice[iepsilon],iepsilon*dele);
 		iT0=-1;
 		do{
 			iT0+=1;
 			iT1=iT0+1;
-		}while(EpsilonvsTvec[iT1]<epsilon_target);
+		}while(Tvec[iT1]<Tlattice[iepsilon]);
 		T0=Tvec[iT0];
 		T1=Tvec[iT1];
 		chi0=ChivsTvec[iT0];
 		chi1=ChivsTvec[iT1];
-		dE=EpsilonvsTvec[iT1]-EpsilonvsTvec[iT0];
-		Tlattice[iepsilon]=(T0*(EpsilonvsTvec[iT1]-epsilon_target)+T1*(epsilon_target-EpsilonvsTvec[iT0]))/dE;
-		chilattice_us[iepsilon]=(chi0*(EpsilonvsTvec[iT1]-epsilon_target)+chi1*(epsilon_target-EpsilonvsTvec[iT0]))/dE;
+		dT=Tvec[iT1]-Tvec[iT0];
+		chilattice_us[iepsilon]=(chi0*(Tvec[iT1]-Tlattice[iepsilon])+chi1*(Tlattice[iepsilon]-Tvec[iT0]))/dT;
+		//printf("epsilon=%g: iT0=%d, iT1=%d, T0=%g, T1=%g, Tlattice=%g, chi0=%g, chi1=%g, chi=%g\n",
+		//elattice[iepsilon],iT0,iT1,T0,T1,Tlattice[iepsilon],chi0,chi1,chilattice_us[iepsilon]);
 	}
+
 	
 ///////////////////////////////////////////////
 		// Now read in lattice data for chi_ss
-	
-	printf("----- chi_ud --------\n");
-	fptr=fopen("latticedata_claudia/c2S.cont","r");
-	Tvec.clear();
+
+	printf("----- chi_ss --------\n");
+	fptr=fopen("latticedata_claudia/c2s.cont","r");
+	Tvec.clear(); ChivsTvec.clear();
 	do{
 		fscanf(fptr,"%lf %lf %lf",&Tread,&chiread,&error);
-		fgets(dummy,200,fptr);
+		fgets(dummy,300,fptr);
 		if(!feof(fptr) && Tread<=400.00001){
 			Tvec.push_back(Tread/1000.0);
 			chiread=chiread*pow(Tread/HBARC,3);
@@ -159,21 +177,37 @@ int main(){
 	fclose(fptr);
 	
 	for(iepsilon=0;iepsilon<NE;iepsilon++){
-		epsilon_target=dele*iepsilon;
+		//printf("Ttarget=%g, e=%g\n",Tlattice[iepsilon],iepsilon*dele);
 		iT0=-1;
 		do{
 			iT0+=1;
 			iT1=iT0+1;
-		}while(EpsilonvsTvec[iT1]<epsilon_target);
+		}while(Tvec[iT1]<Tlattice[iepsilon]);
 		T0=Tvec[iT0];
 		T1=Tvec[iT1];
 		chi0=ChivsTvec[iT0];
 		chi1=ChivsTvec[iT1];
-		dE=EpsilonvsTvec[iT1]-EpsilonvsTvec[iT0];
-		Tlattice[iepsilon]=(T0*(EpsilonvsTvec[iT1]-epsilon_target)+T1*(epsilon_target-EpsilonvsTvec[iT0]))/dE;
-		chilattice_ss[iepsilon]=(chi0*(EpsilonvsTvec[iT1]-epsilon_target)+chi1*(epsilon_target-EpsilonvsTvec[iT0]))/dE;
-	}
+		dT=Tvec[iT1]-Tvec[iT0];
+		chilattice_ss[iepsilon]=(chi0*(Tvec[iT1]-Tlattice[iepsilon])+chi1*(Tlattice[iepsilon]-Tvec[iT0]))/dT;
+		//printf("epsilon=%g: iT0=%d, iT1=%d, T0=%g, T1=%g, Tlattice=%g, chi0=%g, chi1=%g, chi=%g\n",
+		//elattice[iepsilon],iT0,iT1,T0,T1,Tlattice[iepsilon],chi0,chi1,chilattice_ss[iepsilon]);
+	}	
 	
+
+///////////////////////////////////////////////
+		// Write lattice vs epsilon
+	fptr=fopen("LatticeVsEpsilon.txt","w");
+	fprintf(fptr,"#epsilon     T           chi_uu          chi_ud             chi_us             chi_ss\n");
+	for(iepsilon=0;iepsilon<NE;iepsilon++){
+		epsilon_target=dele*iepsilon;
+		fprintf(fptr,"%15.8e %15.8e %15.8e %15.8e %15.8e %15.8e\n",epsilon_target,Tlattice[iepsilon],
+		chilattice_uu[iepsilon],chilattice_ud[iepsilon],chilattice_us[iepsilon],chilattice_ss[iepsilon]);
+		printf("%8.4f %6.4f %8.5f %8.5f %8.5f %8.5f\n",epsilon_target,Tlattice[iepsilon],
+		chilattice_uu[iepsilon]/slattice[iepsilon],chilattice_ud[iepsilon]/slattice[iepsilon],
+		chilattice_us[iepsilon]/slattice[iepsilon],chilattice_ss[iepsilon]/slattice[iepsilon]);
+	}
+	fclose(fptr);
+
 	return 0;
 }
 
@@ -200,7 +234,6 @@ void GetLatticeEoS(vector<double> &Parray,vector<double> &sarray,vector<double> 
 		Parray[iT+1]=pow(T,4)*poverT4;
 		I=pow(T,4.0)*exp(-(h1/t)-h2/(t*t))*( h0+f0*(tanh(f1*t+f2)+1.0)/(1.0+g1*t+g2*t*t) );
 		epsilonarray[iT+1]=I+3.0*Parray[iT+1];
-		sarray[iT+1]=(Parray[iT+1]+epsilonarray[iT+1])/T;
 		//printf("%5.1f s=%g\n",T,sarray[iT]*hbarc3);
 		if(iT>0){
 			cs2=(Parray[iT+1]-Parray[iT-1])/(epsilonarray[iT+1]-epsilonarray[iT-1]);
@@ -213,6 +246,7 @@ void GetLatticeEoS(vector<double> &Parray,vector<double> &sarray,vector<double> 
 		//epsilonarray[iT]/(pow(T,4)),Parray[iT]/(pow(T,4)),sarray[iT]/(pow(T,3)));
 		Parray[iT]=Parray[iT]*hbarc3/1000.0;
 		epsilonarray[iT]=epsilonarray[iT]*hbarc3/1000.0;
+		sarray[iT]=(Parray[iT]+epsilonarray[iT])*1000.0/T;
 	}
 	fclose(fptr);
 
