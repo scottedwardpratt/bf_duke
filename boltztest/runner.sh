3dd7d87eee43e2866a6f2f18060670d890c3d4eb
scottedwardpratt <prattsc@msu.edu>
2024-10-18 09:48:54 -0400

    yada yada

--- boltztest/runner.sh
+++ boltztest/runner.sh
@@ -7,6 +7,6 @@ case $# in
 		run_number=$1
 		subrun_number=$2
 		rm -f logfiles/run${run_number}_subrun${subrun_number}.txt
-		../bin/boltz ${run_number} ${subrun_number} > logfiles/run${run_number}_subrun${subrun_number}.txt
+		../bin/boltz ${run_number} ${subrun_number} > logfiles/runscript_run${run_number}_subrun${subrun_number}.txt
 
 esac
