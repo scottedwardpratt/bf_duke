8c89f637382803646058ddb35bd11c0658265e99
scottedwardpratt <prattsc@msu.edu>
2024-11-08 13:32:29 -0500

    yada yada

---
+++ boltzudstest/bigrunner.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+case $# in
+0|1|2)
+	echo "Usage: bigrunner.sh run_number first_subrun last_subrun // runs from i=ifirst to <=ifinal" >> crap.txt;
+	exit 1 ;;
+3)
+	cd /home/scott/git/bf_duke/boltzudstest
+	run_number=$1
+	first_subrun=$2
+	last_subrun=$3
+	for((isub=${first_subrun};isub<=${last_subrun};isub++)) do
+		`runner.sh ${run_number} ${isub}` &
+	done
+
+esac
