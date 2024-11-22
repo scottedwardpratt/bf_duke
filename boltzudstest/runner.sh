93e23883d3395f9e45fc91c36def2224d866d108
scottedwardpratt <prattsc@msu.edu>
2024-11-22 13:23:45 -0500

    yada yada

--- boltzudstest/runner.sh
+++ boltzudstest/runner.sh
@@ -7,7 +7,7 @@ case $# in
 		run_number=$1
 		subrun_number=$2
 		rm -f logfiles/run${run_number}_subrun${subrun_number}.txt
-		`stdbuf -oL ../bin/boltzuds ${run_number} ${subrun_number} > logfiles/run${run_number}_subrun${subrun_number}.txt` &
+		`stdbuf -oL ../bin/boltzuds ${run_number} ${subrun_number} > logfiles/run${run_number}_subrun${subrun_number}_out.txt` &
 		echo job submitted for run_number=$1, subrun_number=$2
 
 esac
